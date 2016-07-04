79be174a291c54ce1f52069a3ca8d3fc36ce0e3f
tom kast <tomkast@gmail.com>
2016-07-04 14:33:43 +0200

    site is up

---
+++ synch_up.bat
@@ -0,0 +1,5 @@
+pscp -r -pw Gm1-)@s. docs wordsnle@wordzoo:public_html
+@REM pscp -r -pw Gm1-)@s. images wordsnle@wordzoo:public_html
+pscp -r -pw Gm1-)@s. owl wordsnle@wordzoo:public_html
+pscp -r -pw Gm1-)@s. *.html wordsnle@wordzoo:public_html
+pscp -r -pw Gm1-)@s. *.css wordsnle@wordzoo:public_html
